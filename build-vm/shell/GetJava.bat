1f7623039aae498020442553899634a9f8ee045b
shaowns <shaown@csebuet.org>
2015-11-24 18:36:21 -0500

    Added jdk 1.6 script

---
+++ build-vm/shell/GetJava.bat
@@ -0,0 +1,4 @@
+if not exist "C:\Tools" mkdir "C:\Tools"
+echo "Downloading JDK 1.6"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/SoftwareEngineeringToolDemos/FSE-2012-SecuriTAS/raw/master/build-vm/files/jdk-6u32-windows-i586.exe','C:\Tools\jdk.exe')"
+start /WAIT "C:\Tools\jdk.exe" /s
--- build-vm/shell/SetEnvVar.bat
+++ build-vm/shell/SetEnvVar.bat
@@ -1,2 +1,2 @@
-setx /M JAVA_HOME "C:\Program Files\Java\jdk1.7.0_79"
-setx /M PATH "%PATH%;C:\Program Files\Java\jdk1.7.0_79\bin"
+setx /M JAVA_HOME "C:\Program Files\Java\jdk1.6.0_32"
+setx /M PATH "%PATH%;C:\Program Files\Java\jdk1.6.0_32\bin;C:\MySQL\MySQL Server 5.7\bin"
