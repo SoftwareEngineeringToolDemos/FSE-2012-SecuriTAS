9673c81e0aedd2717166b1629cfefbd83776caa8
shaowns <shaown@csebuet.org>
2015-11-26 05:12:02 -0500

    Changed graphviz installation and vagrantfile ordering

--- build-vm/shell/GetGraphviz.bat
+++ build-vm/shell/GetGraphviz.bat
@@ -1,1 +1,4 @@
-%ALLUSERSPROFILE%\chocolatey\bin\choco install graphviz -y
+if not exist "C:\Tools" mkdir "C:\Tools"
+if not exist "C:\Graphviz" mkdir "C:\Graphviz"
+echo "Downloading Graphviz"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "(New-Object Net.WebClient).DownloadFile('http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.zip','C:\Tools\Graphviz.zip');(new-object -com shell.application).namespace('C:\Graphviz\').CopyHere((new-object -com shell.application).namespace('C:\Tools\Graphviz.zip').Items(),16)"
