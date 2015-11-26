c02488e7488264c0999bc714fc162a169af5220b
shaowns <shaown@csebuet.org>
2015-11-26 16:08:28 -0500

    Updated Graphviz download link

--- build-vm/shell/GetGraphviz.bat
+++ build-vm/shell/GetGraphviz.bat
@@ -1,4 +1,4 @@
 if not exist "C:\Tools" mkdir "C:\Tools"
 if not exist "C:\Graphviz" mkdir "C:\Graphviz"
 echo "Downloading Graphviz"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "(New-Object Net.WebClient).DownloadFile('http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.zip','C:\Tools\Graphviz.zip');(new-object -com shell.application).namespace('C:\Graphviz\').CopyHere((new-object -com shell.application).namespace('C:\Tools\Graphviz.zip').Items(),16)"
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/SoftwareEngineeringToolDemos/FSE-2012-SecuriTAS/raw/master/build-vm/files/graphviz-2.38.zip','C:\Tools\Graphviz.zip');(new-object -com shell.application).namespace('C:\Graphviz\').CopyHere((new-object -com shell.application).namespace('C:\Tools\Graphviz.zip').Items(),16)"
